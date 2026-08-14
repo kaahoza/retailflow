304771ed540783c4e22a33350f9c577d7ffba0a8
kaahoza <kaahoza@gmail.com>
2026-08-14 20:57:38 +0200

    Initial commit

---
+++ src/main/java/com/anele/retailflow/RetailflowApplication.java
@@ -0,0 +1,13 @@
+package com.anele.retailflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anele/retailflow/RetailflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anele.retailflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
