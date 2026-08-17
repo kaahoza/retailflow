36e9232ccdc003a2224c02aae8400e90e9f29860
kaahoza <kaahoza@gmail.com>
2026-08-18 00:06:52 +0200

    fix: add chmod +x for mvnw in Dockerfile, ensure docker-compose healthcheck present

--- src/main/java/com/anele/retailflow/RetailflowApplication.java
+++ src/main/java/com/anele/retailflow/RetailflowApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RetailflowApplication {
+public class
+RetailflowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetailflowApplication.class, args);
